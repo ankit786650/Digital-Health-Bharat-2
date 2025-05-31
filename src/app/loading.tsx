0c0fc5af259ea19dec1619c8894df14be58c9295
Ankit <ankit@raktacure.ai>
2025-05-31 04:13:19 +0000

    why switching in pages are lagging

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="flex flex-1 items-center justify-center h-full">
+      <div className="text-lg font-semibold text-muted-foreground">
+        Loading...
+      </div>
+    </div>
+  );
+}
